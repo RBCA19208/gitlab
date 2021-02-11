6f167c0ea7bf3043b40bee3e01cf49fb4aa79063
sysadmin <sysadmin@bcalab1-m11>
2021-02-11 10:16:01 +0530

    added int x

--- src/gitlab/student.java
+++ src/gitlab/student.java
@@ -1,5 +1,12 @@
 package gitlab;
 
 public class student {
+	
+	public static void main(String[] args) {
+			//TODO Auto-generated methyod stub
+			System.out.println("hello world");
+			
+			int x;
+	}
 
 }
